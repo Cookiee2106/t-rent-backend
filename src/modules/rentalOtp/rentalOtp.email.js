a9c0bd3ef19827f14afdb3b50ca40da4250997a3
Pham Thinh <phamdoanthinh60@gmail.com>
2026-06-23 03:06:55 +0700

    fix: reduce nodemailer timeout to 5s to prevent request hanging on Render

--- src/modules/rentalOtp/rentalOtp.email.js
+++ src/modules/rentalOtp/rentalOtp.email.js
@@ -6,6 +6,9 @@ const transporter = nodemailer.createTransport({
     user: process.env.SMTP_EMAIL,
     pass: process.env.SMTP_APP_PASSWORD,
   },
+  connectionTimeout: 5000,
+  greetingTimeout: 5000,
+  socketTimeout: 5000,
 });
 
 /**
