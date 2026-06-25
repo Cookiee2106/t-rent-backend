c38ca5e2e6f500aa98efa108c8ef1d6b966cbba1
Nguyen Dai Tai <daitai21062004@gmail.com>
2026-06-26 01:00:25 +0700

    Clean up old structure on develop before merging

--- src/modules/deviceModels/deviceModelRoutes.js
+++
@@ -1,7 +0,0 @@
-const router = require("express").Router();
-const deviceModelController = require("./deviceModelController");
-
-router.get("/", deviceModelController.getProductModels);
-router.get("/:id", deviceModelController.getProductModelDetail);
-
-module.exports = router;
--- src/modules/rentalTerms/rentalTermRoutes.js
+++
@@ -1,8 +0,0 @@
-const router = require("express").Router();
-const authMiddleware = require("../../middlewares/authMiddleware");
-const rentalTermController = require("./rentalTermController");
-
-router.get("/current", rentalTermController.getCurrentTerms);
-router.post("/accept", authMiddleware, rentalTermController.acceptTerms);
-
-module.exports = router;
